b4dd5449f2e4dbce9f392894b51ac2b0885a10b9
Mallu Harshitha Reddy <98054895+harshitha082@users.noreply.github.com>
2022-10-29 00:03:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const fs= require("fs");
+let homeContent="";
+let fileContent="";
+let reg="";
+fs.readFile("home.html",
+(err,home) => {
+if(err){
+    throw err;
+}
+homeContent=home;
+});
+
+fs.readFile("project.html",
+(err,fil) => {
+    if(err)
+    {
+        throw err;
+    }
+   fileContent=fil;
+})
+fs.readFile("registration.html",
+(err,regf) => {
+    if(err)
+    {
+        throw err;
+    }
+ reg=regf;
+})
+let args=require("minimist")(process.argv.slice(2));
+http.createServer((request,response) =>{
+    let url =request.url
+    response.writeHeader(200,{"Content-type":"text/html"})
+    switch (url)
+    {
+        case "./project":
+            response.write(fileContent)
+            response.end();
+            break
+        case "/registration":
+            response.write(reg)
+            response.end();
+            break;
+        default:
+            response.write(homeContent)
+            response.end();
+            break;
+    }
+}).listen(args["port"]);
